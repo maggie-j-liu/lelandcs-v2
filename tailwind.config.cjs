5921285fe70e3567656ff802c6a5d70b08b3e85d
maggie-j-liu <maggie.j.liu@gmail.com>
2022-10-06 15:48:38 -0700

    initial site

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Rubik", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         fuchsia: {
           ...colors.fuchsia,
